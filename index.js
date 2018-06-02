18b3ec8ad380f45228ee22f3331d462c3b795a9c
Sam Sanders <sam-sanders@hotmail.co.uk>
2018-06-02 19:03:43 +1200

    working, but timere to slow down is not

--- index.js
+++ index.js
@@ -5,44 +5,62 @@
 
 // const board = require('./board.js')
 let clear = require('clear')
-
 const x = 0
-
-// const arr = [
-//   [x, x, x, x, x, x, x, x, x, x, x, x],
-//   [x, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, x],
-//   [x, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, x],
-//   [x, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, x],
-//   [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
-//   [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
-//   [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
-//   [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
-//   [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
-//   [x, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, x],
-//   [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
-//   [x, x, x, x, x, x, x, x, x, x, x, x]
-// ]
-
 const arr = [
-  [x, x, x, x, x],
-  [x, 0, 1, 0, x],
-  [x, 0, 1, 0, x],
-  [x, 0, 1, 0, x],
-  [x, x, x, x, x]
+  [x, x, x, x, x, x, x, x, x, x, x, x],
+  [x, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, x, x, x, x, x, x, x, x, x, x, x]
 ]
-// checks first arr for alive cells
-let prints = 0
+const newArr = [
+  [x, x, x, x, x, x, x, x, x, x, x, x],
+  [x, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, x],
+  [x, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, x],
+  [x, x, x, x, x, x, x, x, x, x, x, x]
+]
 
+function gameStart () { // Where it all begins
+  scanner()
+}
+// checks first arr for alive cells
+// let prints = 0
 function scanner (i, j, prints) {
-  for (let i = 1; i < 5; i++) {
-    for (let j = 1; j < 5; j++) {
-      countSur(i, j)
-    }
-  }
   print()
-  if (prints <= 5) {
-    prints += 1
-    scanner(prints)
+  for (let t = 0; t < 10000; t++) {
+    for (let i = 1; i < 11; i++) {
+      for (let j = 1; j < 11; j++) {
+        countSur(i, j)
+      }
+    }
+    print()
+    updateArr()
+  }
+  // if (prints <= 5) {
+  //   prints++
+  //   scanner(prints)
+  // }
+}
+function updateArr () {
+  for (let z = 0; z < 11; z++) {
+    for (let c = 0; c < 411; c++) {
+      arr[z][c] = newArr[z][c]
+    }
   }
 }
 
@@ -53,40 +71,45 @@ function countSur (y, j) {
     arr[y][j - 1], arr[y][j + 1],
     arr[y + 1][j - 1], arr[y + 1][j], arr[y + 1][j + 1]
   ]
-  newArr = JSON.parse(JSON.stringify(questionPool))
   const reducer = (accu, cur) => accu + cur
   let surrounds = surArr.reduce(reducer)
-  console.log(surrounds)
   lifeOrDeath(y, j, surrounds)
 }
 
 function lifeOrDeath (y, j, surrounds) {
-  print()
-
   if ((arr[y][j] === 0) && (surrounds === 3)) { //        If cell is dead and surrounds are exactly 3
-    (arr[y][j] = 1) && (j += 1) //                        cell is born / scanner starts on the next cell
+    (newArr[y][j] = 1) //                                 cell is born / scanner starts on the next cell
   } else if ((surrounds < 2) || (surrounds > 3)) { //     If cel has less than 2 around it
-    (arr[y][j] = 0) && (j += 1) //                        cell ends itself due to loneliness / scanner starts on the next cell
+    (newArr[y][j] = 0) //                                 cell ends itself due to loneliness / scanner starts on the next cell
   } else if ((arr[y][j] === 1) && (surrounds === (2 || 3))) { // If cell is alive and has 2/3 surroundsing friends
-    (arr[y][j] = 1) && (j += 1)//                                cell lives for another day / scanner starts on the next cell
+    (newArr[y][j] = 1) //                                 cell lives for another day / scanner starts on the next cell
   }
-  // } else if (surrounds > 3) { //                                 If cell has more than three neighbors,
-  //   (arr[y][j] = 0) && (j += 1) //                cell ends itself, depressed by lack of space / scanner starts on the next cell
-  // }
 }
 
 function print () {
-  for (let f = 0; f < 5; f++) {
-    console.log(arr[f])
+  for (let f = 0; f < 12; f++) {
+    show(newArr[f])
   }
-  // clear()
+  clear()
+  time()
+  show('\n')
 }
 
-function gameStart () { // Where it all begins
-  scanner()
+function show (a) {
+  console.log(a)
 }
 
 gameStart()
+
+function time () {
+  let timeLeft = 10
+  let clock = setInterval(function () {
+    timeLeft--
+    if (timeLeft === 0) {
+      clearInterval(clock)
+    }
+  }, 1000)
+}
 
 module.exports = {
   gameStart,
